ab27bba03f2604e7a346ee395a2c3fb68319c9b8
Srinivasan Vijayarangan <srinivasan@cmu.edu>
2018-11-09 15:54:02 -0500

    Added the epoch loop

--- specialized_superpoint.py
+++ specialized_superpoint.py
@@ -16,6 +16,26 @@ from base_model import *
 from tensorboardX import SummaryWriter
 from torch.optim.lr_scheduler import *
 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+def save_model(model,epoch,dist,runid):
+    path = os.getcwd()
+    outdir = os.path.join(path,str(runid))
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
+        
+    filename1 = outdir + '/e_'+str(epoch)+'_a_{0:.4f}'.format(dist)+'.model'
+    net = model.cpu()
+    torch.save(net,filename1)
+    
+    filename2 = outdir + '/e_'+str(epoch)+'_a_{0:.4f}'.format(dist)+'.dict'
+    torch.save(net.state_dict(), filename2)
+    
+    print('\t\t\t\t\t\t\tSaving model to: {0} and dict to: {1}'.format(filename1,filename2))
+
+    model.to(DEVICE)
+
+    
 def train(params):
     
     writer = SummaryWriter(log_dir='runs/'+params.runid)
@@ -29,45 +49,56 @@ def train(params):
                               batch_size=params.batch,shuffle=True,collate_fn=collate_fn)
     
     model = SuperPointNet()
-    optimizer = torch.optim.Adam(model.parameters(),lr=params.lr,weight_decay=params.weightdecay)
-    criterion = nn.CrossEntropyLoss() #reduction='elementwise_sum')
+    model.to(DEVICE)
     
-    for batch_idx, (imgs,pix_locs,didx) in enumerate(train_loader):
+    optimizer = torch.optim.Adam(model.parameters(),lr=params.lr,weight_decay=params.weightdecay)
+    
+    criterion = nn.CrossEntropyLoss() #reduction='elementwise_sum')
         
-        #bnum = imgs.shape[0]
-        #h,w = imgs[0].shape
+    for e in range(params.epoch):
+        
+        epoch_loss = 0
+        img_count = 0
+        
+        for batch_idx, (imgs,pix_locs,didx) in enumerate(train_loader):
 
-        ipt,desc = model(imgs.float().unsqueeze(1))
-        #ipt bnum x 65 x hc x wc
-        bnum, dims, hc, wc = ipt.shape
-        
-        #for pixels in each image, divide by eight to find which sector it belongs to
-        #modulo by 8 to find the value in each sector
-        dim_locs = [(torch.tensor(pix_locs[i])/8).int() for i in range(len(pix_locs))]
-        dim_val = [(torch.tensor(pix_locs[i])%8).int() for i in range(len(pix_locs))]
-        
-        #create the encoded vector
-        pts = torch.ones(bnum,hc,wc)*64 #initialize to dustbin values which will be cleared if there is a true value
+            ipt,desc = model(imgs.float().unsqueeze(1).to(DEVICE))
+            #ipt bnum x 65 x hc x wc
+            bnum, dims, hc, wc = ipt.shape
+
+            #for pixels in each image, divide by eight to find which sector it belongs to
+            #modulo by 8 to find the value in each sector
+            dim_locs = [(torch.tensor(pix_locs[i])/8).int() for i in range(len(pix_locs))]
+            dim_val = [(torch.tensor(pix_locs[i])%8).int() for i in range(len(pix_locs))]
+
+            #create the encoded vector
+            pts = torch.ones(bnum,hc,wc)*64 #initialize to dustbin values which will be cleared if there is a true value
+
+            #TODO: Make this faster
+            #for each image in the batch
+            for b in range(len(dim_locs)):
+
+                #for each pixel location
+                for i in range(len(dim_locs[b])):
+                    pts[b,dim_locs[b][i][0],dim_locs[b][i][1]] = 8*dim_val[b][i][0] +  dim_val[b][i][1]
+
+            pts = pts.long()
+            #pdb.set_trace()
+
+            #probs bnum x 65 x hc x w
+            loss = criterion(ipt.to(DEVICE),pts.long().to(DEVICE))
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            epoch_loss += loss.item()
             
-        #TODO: Make this faster
-        #for each image in the batch
-        for b in range(len(dim_locs)):
-            
-            #for each pixel location
-            for i in range(len(dim_locs[b])):
-                pts[b,dim_locs[b][i][0],dim_locs[b][i][1]] = 8*dim_val[b][i][0] +  dim_val[b][i][1]
+            print('\x1b[2K\rEpoch {0} Loss:{1:.3f} Batch idx:{2}'.format(e+1,loss.item(),batch_idx),end='\r')
+            img_count += bnum
+
+        print('\x1b[2K\rEnd of epoch {0} Loss:{1:.5f}'.format(e+1,epoch_loss))
         
-        pts = pts.long()
-        #pdb.set_trace()
-
-        #probs bnum x 65 x hc x w
-        loss = criterion(ipt,pts)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        print('Loss:{0:.3f}'.format(loss.item()))
-              
+        save_model(model,e,epoch_loss,params.runid)
         #probs = softmax(ipt)
 
         #probs bnum x 64 x hc x wc
@@ -86,6 +117,7 @@ def main():
     parser.add_argument('--dev',action='store_true')
     parser.add_argument('--test',action='store_true')
     parser.add_argument('--validate',action='store_true')
+    parser.add_argument('--epoch',default=1000,type=int)
     parser.add_argument('--weightdecay',default=1e-6,type=float)
     parser.add_argument('--lr',default=1e-3,type=float)
     parser.add_argument('--batch',default=64,type=int)
