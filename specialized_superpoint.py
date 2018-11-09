cf00591cdcac71359aac02ce9036dd381129c89d
Srinivasan Vijayarangan <srinivasan@cmu.edu>
2018-11-09 15:31:04 -0500

    Coded in the loss function

--- specialized_superpoint.py
+++ specialized_superpoint.py
@@ -32,20 +32,44 @@ def train(params):
     optimizer = torch.optim.Adam(model.parameters(),lr=params.lr,weight_decay=params.weightdecay)
     criterion = nn.CrossEntropyLoss() #reduction='elementwise_sum')
     
-    for batch_idx, (imgs,pts,didx) in enumerate(train_loader):
+    for batch_idx, (imgs,pix_locs,didx) in enumerate(train_loader):
         
-        bnum = imgs.shape[0]
-        h,w = imgs[0].shape
+        #bnum = imgs.shape[0]
+        #h,w = imgs[0].shape
 
         ipt,desc = model(imgs.float().unsqueeze(1))
-        #pt bnum x 65 x hc x wc
+        #ipt bnum x 65 x hc x wc
+        bnum, dims, hc, wc = ipt.shape
         
-        pdb.set_trace()
+        #for pixels in each image, divide by eight to find which sector it belongs to
+        #modulo by 8 to find the value in each sector
+        dim_locs = [(torch.tensor(pix_locs[i])/8).int() for i in range(len(pix_locs))]
+        dim_val = [(torch.tensor(pix_locs[i])%8).int() for i in range(len(pix_locs))]
+        
+        #create the encoded vector
+        pts = torch.ones(bnum,hc,wc)*64 #initialize to dustbin values which will be cleared if there is a true value
+            
+        #TODO: Make this faster
+        #for each image in the batch
+        for b in range(len(dim_locs)):
+            
+            #for each pixel location
+            for i in range(len(dim_locs[b])):
+                pts[b,dim_locs[b][i][0],dim_locs[b][i][1]] = 8*dim_val[b][i][0] +  dim_val[b][i][1]
+        
+        pts = pts.long()
+        #pdb.set_trace()
 
         #probs bnum x 65 x hc x w
         loss = criterion(ipt,pts)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        print('Loss:{0:.3f}'.format(loss.item()))
+              
         #probs = softmax(ipt)
-        
+
         #probs bnum x 64 x hc x wc
         #probs = probs[:,:-1,:,:]
         
